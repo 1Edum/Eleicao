1f14a21fe806aed10ac8b96f14df2fabb63d21f4
1Edum <eduardomatosdesouza2@gmail.com>
2024-07-13 17:34:14 -0300

    feat: update page initial

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,17 @@
+import Input from "./components/input";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="h-[92vh] flex flex-col items-center justify-center">
+      <section className="bg-tertiary flex flex-col px-10 py-20 gap-12">
+        <h1 className="text-center text-3xl">Login</h1>
+        <div className="flex flex-col gap-2">
+          <Input text="Email"/>
+          <Input text="Senha"/>
+        </div>
+        <button className="bg-primary text-white py-2">Entrar</button>
+      </section>
+
     </main>
 
     
